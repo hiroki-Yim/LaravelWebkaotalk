8fa774684ebac67130f7b147b5b713cdd3023afc
hiroki-Yim <imhk_@naver.com>
2018-12-06 02:25:03 +0900

    Seed Test

--- database/factories/FriendFactory.php
+++ database/factories/FriendFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Friend::class, function (Faker $faker) {
     return [
-        'user_id'=>$faker->numberBetween($min = 1, $max = 30),
-        'friend_id' => $faker->numberBetween($min = 1, $max = 30),
+        'user_id'=>$faker->numberBetween($min = 15, $max = 24),
+        'friend_id' => $faker->numberBetween($min = 15, $max = 24),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(BoardsTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(FriendsTableSeeder::class);
+        //$this->call(BoardsTableSeeder::class);
+        
         
     }
 }
